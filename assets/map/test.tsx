060869cbfdf1c8aafa10c81cdf3c74a5bc0951da
zhorander <elias_50@live.com>
2019-03-30 15:42:13 -0600

    added initial assets

---
+++ assets/map/test.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="RogueDungeon" tilewidth="16" tileheight="16" tilecount="128" columns="8">
+ <image source="../RogueDungeon.png" width="128" height="256"/>
+</tileset>
